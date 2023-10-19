9e850da0f18ab7357e0c243ec47e057555eb8c39
hikidy <138530988+hikidy@users.noreply.github.com>
2023-10-19 19:13:28 -0400

    Update config.js

--- aps-plus-plus-main/server/config.js
+++ aps-plus-plus-main/server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "localhost:3000",
+    host: "prague-io.onrender.com",
 
     // Which port to run the web server on.
     port: 3000,
